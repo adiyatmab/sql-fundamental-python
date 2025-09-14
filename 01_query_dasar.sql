a487cd9a15c205e9a52b64cd4b5e90379ce7c699
Adiyatma Buyung <buyungo415@gmail.com>
2025-09-14 17:59:51 +0700

    Selesai : Latihan Operator Pembanding Lainnya: IN, BETWEEN, LIKE

--- 01_query_dasar.sql
+++ 01_query_dasar.sql
@@ -18,3 +18,21 @@ SELECT * FROM pegawai ORDER BY gaji DESC;
 
 -- Memunculkan nama dan jabatan dari pegawai urut berdasarkan A-Z jabatannya
 select nama, jabatan from pegawai order by jabatan asc
+
+-- Penggunaan IN lebih mudah dan singkat daripada OR
+select * from pegawai where id in ('1','3','5')
+
+-- Mencari pegawai yang namanya diawali dengan huruf 'A'
+SELECT * FROM pegawai WHERE nama LIKE 'A%';
+
+-- Mencari pegawai yang jabatannya diakhiri dengan kata 'er'
+SELECT * FROM pegawai WHERE jabatan LIKE '%er';
+
+-- Mencari pegawai yang namanya memiliki huruf 'n' di mana saja
+SELECT * FROM pegawai WHERE nama LIKE '%n%';
+
+-- Mencari semua pegawai yang gajinya KOSONG
+SELECT * FROM pegawai WHERE gaji IS NULL;
+
+-- Mencari semua pegawai yang gajinya TIDAK KOSONG
+SELECT * FROM pegawai WHERE gaji IS NOT NULL;
